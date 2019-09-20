3553f9c5e3b01664c31b48cf02332d92a6ba4995
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-20 13:42:30 -0400

    and names,py is running in less than 6 seconds with the correct answer

--- names/names.py
+++ names/names.py
@@ -11,10 +11,27 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# original code, ran in 15.5 seconds:
+#for name_1 in names_1:
+    #for name_2 in names_2:
+        #if name_1 == name_2:
+            #duplicates.append(name_1)
+
+
+def find_diff(l1, l2):
+    dupes = [i for i in l1 + l2 if i in l1 and i in l2]
+    dupe_list = []
+    for i in dupes:
+        if i not in dupe_list:
+            dupe_list.append(i)
+    return dupe_list
+
+
+duplicates = find_diff(names_1, names_2)
+
+# runtime between 4.8 seconds and 5.5 seconds! *hands_party* I don't think it was exactly how I was supposed to do it, but it works!
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
